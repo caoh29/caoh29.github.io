0218d636f32662c672ec1901f8589794054246b7
Camilo Ordoñez <cronox20@gmail.com>
2023-08-10 13:13:50 -0500

    adding routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './pages/Home/HomePage';
+import InnerPage from './pages/Inner/InnerPage';
+import ErrorPage from './pages/Error/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/inner",
+    element: <InnerPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/Inner/InnerPage.tsx
@@ -0,0 +1,5 @@
+export default function InnerPage() {
+  return (
+    <div>InnerPage</div>
+  )
+}
