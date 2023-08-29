f2b7fae07823230e78aed93039f798346b8a7b4f
Camilo Ordoñez <cronox20@gmail.com>
2023-08-29 11:58:24 -0500

    addin rnederwithproviders

--- src/store/store.ts
+++ src/store/store.ts
@@ -13,6 +13,8 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 
+export type AppStore = typeof store;
+
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
